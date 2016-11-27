407ff37cfc847e4bcd8cc5d9e797af974a1ecdf2
S. M. Abrar Jahin <abrarjahin@live.com>
2016-11-27 09:04:54 +0600

    AJAX added

--- 2. With AJAX/js/script.js
+++ 2. With AJAX/js/script.js
@@ -1,61 +1,45 @@
-var substringMatcher = function(strs)
+jQuery(document).ready(function($)
 {
-	return function findMatches(q, cb)
+	// Set the Options for "Bloodhound" suggestion engine
+	var engine = new Bloodhound({
+			remote: {
+					url: '/find?search_param=%QUERY%',
+					wildcard: '%QUERY%'
+			},
+			datumTokenizer: Bloodhound.tokenizers.whitespace('search_param'),
+			queryTokenizer: Bloodhound.tokenizers.whitespace
+	});
+
+	$('#typehead_example').typeahead({
+			hint: true,
+			highlight: true,
+			minLength: 1
+	},
 	{
-		//alert(q);
+		source: engine.ttAdapter(),
 
-		var matches, substrRegex;
+		// This will be appended to "tt-dataset-" to form the class name of the suggestion menu.
+		name: 'usersList',
 
-		// an array that will be populated with substring matches
-		matches = [];
-
-		// regex used to determine if a string contains the substring `q`
-		substrRegex = new RegExp(q, 'i');
-
-		// iterate through the pool of strings and for any string that
-		// contains the substring `q`, add it to the `matches` array
-		$.each(strs, function(i, str)
-		{
-			if (substrRegex.test(str))
-			{
-				// the typeahead jQuery plugin expects suggestions to a
-				// JavaScript object, refer to typeahead docs for more info
-				matches.push({ value: str });
-			}
-		});
-
-		cb(matches);
-	};
-};
-
-var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-	'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-	'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-	'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-	'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-	'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-	'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-	'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-	'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-];
- 
-$('#typehead_example').typeahead(
-						{
-							hint:		true,
-							highlight:	true,
-							minLength:	1
-						},
-						{
-							name:		'states',
-							displayKey:	'value',
-							source:		substringMatcher(states)
-						}
-					)
-					.on('typeahead:asyncrequest', function()
+		// the key from the array we want to display (name,id,email,etc...)
+		templates: {
+				empty: [
+						'<div class="list-group search-results-dropdown"><div class="list-group-item">Nothing found.</div></div>'
+					],
+				header: [
+						'<div class="list-group search-results-dropdown">'
+					],
+				suggestion: function (data)
 					{
-						alert("AJAX Start");
-					})
-					.on('typeahead:asynccancel typeahead:asyncreceive', function()
-					{
-						alert("AJAX End");
-					});
+						return '<a href="' + data.profile.username + '" class="list-group-item">' + data.name + '- @' + data.profile.username + '</a>'
+					}
+		}
+	}).on('typeahead:asyncrequest', function()
+				{
+					alert("AJAX Start");
+				})
+				.on('typeahead:asynccancel typeahead:asyncreceive', function()
+				{
+					alert("AJAX End");
+				});
+});

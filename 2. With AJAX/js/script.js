4b4c816800814437939ad0385938c2f4c66235f1
S. M. Abrar Jahin <abrarjahin@live.com>
2016-11-27 11:15:37 +0600

    AJAX working

--- 2. With AJAX/js/script.js
+++ 2. With AJAX/js/script.js
@@ -1,3 +1,4 @@
+/*
 jQuery(document).ready(function($)
 {
 	// Set the Options for "Bloodhound" suggestion engine
@@ -11,10 +12,10 @@ jQuery(document).ready(function($)
 	});
 
 	$('#typehead_example').typeahead({
-			hint: true,
-			highlight: true,
-			minLength: 1
-	},
+				hint: true,
+				highlight: true,
+				minLength: 1
+			},
 	{
 		source: engine.ttAdapter(),
 
@@ -23,23 +24,103 @@ jQuery(document).ready(function($)
 
 		// the key from the array we want to display (name,id,email,etc...)
 		templates: {
-				empty: [
-						'<div class="list-group search-results-dropdown"><div class="list-group-item">Nothing found.</div></div>'
-					],
-				header: [
-						'<div class="list-group search-results-dropdown">'
-					],
-				suggestion: function (data)
+					empty: [
+							'<div class="list-group search-results-dropdown"><div class="list-group-item">Nothing found.</div></div>'
+						],
+					header: [
+							'<div class="list-group search-results-dropdown">'
+						],
+					suggestion: function (data)
+						{
+							return '<a href="' + data.profile.username + '" class="list-group-item">' + data.name + '- @' + data.profile.username + '</a>'
+						}
+				}
+	})
+	.on('typeahead:asyncrequest', function()
+	{
+		alert("AJAX Start");
+	})
+	.on('typeahead:asynccancel typeahead:asyncreceive', function()
+	{
+		alert("AJAX End");
+	});
+});
+*/
+
+$(document).ready(function()
+{
+	$.support.cors = true;
+
+	var engine = new Bloodhound({
+		identify:	function(o)
 					{
-						return '<a href="' + data.profile.username + '" class="list-group-item">' + data.name + '- @' + data.profile.username + '</a>'
-					}
+						return o.id_str;
+					},
+		queryTokenizer: Bloodhound.tokenizers.whitespace,
+		datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name', 'screen_name'),
+		dupDetector:	function(a, b)
+						{
+							return a.id_str === b.id_str;
+						},
+		//prefetch: 'https://typeahead-js-twitter-api-proxy.herokuapp.com' + '/demo/prefetch',
+		remote: {
+			url: 'https://typeahead-js-twitter-api-proxy.herokuapp.com' + '/demo/search?q=%QUERY',
+			wildcard: '%QUERY'
 		}
-	}).on('typeahead:asyncrequest', function()
-				{
-					alert("AJAX Start");
-				})
-				.on('typeahead:asynccancel typeahead:asyncreceive', function()
-				{
-					alert("AJAX End");
-				});
+	});
+
+	// ensure default users are read on initialization
+	//engine.get('1090217586', '58502284', '10273252', '24477185')
+
+	function engineWithDefaults(query_string, sync, async)
+	{
+		if (query_string === '')
+		{
+			sync(engine.get('1090217586', '58502284', '10273252', '24477185'));
+			async([]);
+		}
+
+		else
+		{
+			engine.search(query_string, sync, async);
+		}
+	}
+
+	$('#typehead_example').typeahead({
+				hint:		true,
+				highlight:	true,
+				//menu: $('.typeahead-menu'),
+				minLength: 0,
+				classNames:	{
+					//open: 'is-open',
+					//empty: 'is-empty',
+					//cursor: 'is-active',
+					//suggestion: 'typeahead-suggestion',
+					//selectable: 'typeahead-selectable'
+				}
+			},
+			{
+				source: engineWithDefaults,
+				displayKey:	'screen_name',
+				templates:	{
+							empty: [
+									'<div class="list-group search-results-dropdown"><div class="list-group-item">Nothing found.</div></div>'
+								],
+							header: [
+									'<div class="list-group search-results-dropdown">'
+								],
+							suggestion: function (data)
+								{
+									return '<a href="' + data.created_at + '" class="list-group-item">' + data.name + '- @' + data.status.created_at + '</a>'
+								}
+						}
+			})
+			.on('typeahead:asyncrequest', function()
+			{
+				$('.typeahead-spinner').show();
+			})
+			.on('typeahead:asynccancel typeahead:asyncreceive', function()
+			{
+				$('.typeahead-spinner').hide();
+			});
 });
